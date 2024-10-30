925d14e66cbbcd293a8c63c205d6343fc389a3ad
Yishai Zehavi <33053646+zyishai@users.noreply.github.com>
2024-10-30 15:35:40 +0200

    style: simple ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,61 +17,74 @@ export default function Index() {
     sendOrder({ type: 'game.start' });
   }, []);
 
-  return <ClientOnly>{() => (
-    <div className="flex flex-col h-screen items-center justify-center gap-4 dark:bg-black">
-      <h1 className="text-lg font-semibold tracking-wide">Day #{state.context.day}</h1>
-      <p>{state.context.port}</p>
-      <p>Next Price Update in {state.context.nextPriceChange} days.</p>
-      <hr className="my-3 border-0 h-px bg-gray-400 w-9/12" />
+  return <ClientOnly>{() => 
+    <div className="flex flex-col h-screen items-center justify-center gap-4 bg-slate-800 text-slate-100">
+      <h1 className="text-3xl font-semibold tracking-wide">Day #{state.context.day}</h1>
       <span className="text-xl">Balance: <strong className="tabular-nums">${state.context.balance}</strong></span>
-      <div className="flex gap-3">
-        {(['copper', 'wheat', 'olive'] as const).map(good => (
-          <div className="flex flex-col gap-5 items-center" key={good}>
-            <div className="w-24 h-24 border border-gray-400 rounded-lg flex flex-col justify-center items-center gap-1">
-              <strong className="text-lg tabular-nums">{state.context.inventory[good]}</strong>
-              <p>{good}</p>
-            </div>
-            <button 
-              className="bg-white text-black w-full rounded-md py-1.5 hover:bg-gray-200"
-              onClick={() => sendOrder({ type: 'buy', good, quantity: 1 })}>Buy</button>
-            <button 
-              className="bg-white text-black w-full rounded-md py-1.5 hover:bg-gray-200"
-              onClick={() => sendOrder({ type: 'sell', good, quantity: 1 })}>Sell</button>
-          </div>
-        ))}
-      </div>
       <hr className="my-3 border-0 h-px bg-gray-400 w-9/12" />
-      <table>
+      <table className="table-fixed w-9/12 mx-auto">
         <thead>
-          <tr>
-            <th>Country</th>
-            <th>Copper</th>
-            <th>Wheat</th>
-            <th>Olive</th>
+          <tr className="border-b border-gray-600">
+            <th className="text-left pb-2 w-44"></th>
+            <th className="text-center pb-2">Copper</th>
+            <th className="text-center pb-2">Wheat</th>
+            <th className="text-center pb-2">Olive</th>
           </tr>
         </thead>
         <tbody>
           {ports.map(port => (
-            <tr key={port}>
-              <td>{port}</td>
-              <td className="tabular-nums">${state.context.prices[port].copper}</td>
-              <td className="tabular-nums">${state.context.prices[port].wheat}</td>
-              <td className="tabular-nums">${state.context.prices[port].olive}</td>
+            <tr key={port} className="hover:bg-gray-900 cursor-crosshair">
+              <td className="py-2.5 hover:text-yellow-400 text-left">{port}</td>
+              <td className="py-2.5 hover:text-yellow-400 text-center tabular-nums">${state.context.prices[port].copper}</td>
+              <td className="py-2.5 hover:text-yellow-400 text-center tabular-nums">${state.context.prices[port].wheat}</td>
+              <td className="py-2.5 hover:text-yellow-400 text-center tabular-nums">${state.context.prices[port].olive}</td>
             </tr>
           ))}
+          <tr className="border-t border-gray-400 text-lg">
+            <td className="pt-4 text-left">Time to next update</td>
+            <td colSpan={2}></td>
+            <td className="pt-4 text-center text-lg">{state.context.nextPriceChange} days</td>
+          </tr>
         </tbody>
       </table>
-      <div className="flex gap-3">
-        {ports.filter(port => port !== state.context.port).map(port => (
-          <button key={port} onClick={() => sendOrder({ type: 'travel', to: port })}>{port}</button>
-        ))}
+      <div className="mt-9 w-9/12 flex items-center justify-between">
+        <div>
+          <h2 className="text-xl">{state.context.destination ? `Traveling to ${state.context.destination}..` : 'Travel'}</h2>
+          <div className="mt-3 flex gap-3">
+            {ports.map(port => (
+              <button
+                key={port}
+                className={["text-lg border border-gray-600 py-1 px-5 rounded-md", port === state.context.port ? "bg-white text-black" : "hover:bg-white/10"].join(' ')}
+                onClick={() => sendOrder({ type: 'travel', to: port })}>{port}</button>
+            ))}
+          </div>
+          <div className={["mt-7 flex gap-2 items-center border-t py-3 px-1", state.context.event ? "" : "invisible"].join(' ')}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+            </svg>
+            <p className="">{state.context.event}</p>
+            <button className="ml-auto bg-white text-black w-16 py-1 rounded-md hover:bg-gray-300" onClick={() => sendOrder({ type: 'event.resolve' })}>
+              <span className="text-sm">OK</span>
+            </button>
+          </div>
+        </div>
+        <div className="flex gap-3">
+          {(['copper', 'wheat', 'olive'] as const).map(good => (
+            <div className="flex flex-col gap-5 items-center" key={good}>
+              <div className="w-24 h-24 border border-gray-400 rounded-lg flex flex-col justify-center items-center gap-1">
+                <strong className="text-lg tabular-nums">{state.context.inventory[good]}</strong>
+                <p>{good}</p>
+              </div>
+              <button 
+                className="bg-white text-black w-full rounded-md py-1.5 hover:bg-gray-200"
+                onClick={() => sendOrder({ type: 'buy', good, quantity: 1 })}>Buy</button>
+              <button 
+                className="bg-white text-black w-full rounded-md py-1.5 hover:bg-gray-200"
+                onClick={() => sendOrder({ type: 'sell', good, quantity: 1 })}>Sell</button>
+            </div>
+          ))}
+        </div>
       </div>
-      {state.context.event ? <div className="mt-3 mx-auto w-7/12 flex justify-between items-center border border-gray-500 p-3 rounded-xl">
-        <p>EVENT: {state.context.event}</p>
-        <button
-          className="bg-white hover:bg-gray-300 text-black w-14 py-1 rounded-md"
-          onClick={() => sendOrder({ type: 'event.resolve' })}>OK</button>
-      </div> : null}
     </div>
-  )}</ClientOnly>;
+  }</ClientOnly>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     extend: {
       fontFamily: {
         sans: [
+          "Macondo",
           '"Inter"',
           "ui-sans-serif",
           "system-ui",
