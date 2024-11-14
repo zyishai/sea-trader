b9738042f4b053b6a39ed93563d359d2cd3f24ea
Yishai Zehavi <33053646+zyishai@users.noreply.github.com>
2024-11-14 12:54:01 +0200

    feat: market buy and sell

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -1,33 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { Box, Text, useInput } from "ink";
 import { GameContext } from "./GameContext.js";
-import { calculateTravelTime, Port, ports } from "../store/store.js";
+import { calculateTravelTime, getAvailableStorage, Good, goods, Port, ports } from "../store/store.js";
 import { Tab, Tabs } from "ink-tab";
 import BigText from "ink-big-text";
 import Select from "ink-select-input";
+import { v4 as randomId } from "uuid";
+import { Table } from "@tqman/ink-table";
+import { TextInput } from "@inkjs/ui";
 
 type Tab = "port" | "market" | "shipyard";
+type Message = {
+  id: string;
+  text: string;
+  clearAfter?: number;
+};
 
 export function GameScreen() {
   const context = GameContext.useSelector((snapshot) => snapshot.context);
   const actor = GameContext.useActorRef();
   const healthStatus = context.ship.health >= 100 ? "Perfect" : context.ship.health >= 70 ? "Good" : "Needs repair";
   const [activeTab, setActiveTab] = useState<Tab>("port");
-  const [message, setMessage] = useState<string | undefined>();
+  const [messages, setMessages] = useState<Message[]>([]);
 
+  // Event: "message"
   useEffect(() => {
     const subscription = actor.on("message", (evt) => {
-      setMessage(evt.message);
+      const messageId = randomId();
+      setMessages((msgs) => [
+        ...msgs,
+        {
+          id: messageId,
+          text: evt.message,
+          clearAfter: evt.timeout === "auto" ? 2000 : evt.timeout,
+        },
+      ]);
+      if (evt.timeout) {
+        setTimeout(
+          () => {
+            setMessages((msgs) => (msgs.length > 0 ? msgs.filter((msg) => msg.id !== messageId) : msgs));
+          },
+          evt.timeout === "auto" ? 2000 : evt.timeout,
+        );
+      }
     });
 
     return subscription.unsubscribe;
   }, []);
 
+  // Event: "clearMessages"
   useEffect(() => {
-    const timeoutId = setTimeout(setMessage, 2000);
+    const subscription = actor.on("clearMessages", () => {
+      setMessages([]);
+    });
 
-    return () => clearTimeout(timeoutId);
-  }, [message]);
+    return subscription.unsubscribe;
+  }, []);
 
   useInput((input, key) => {
     if (key.escape) {
@@ -37,8 +65,14 @@ export function GameScreen() {
 
   return (
     // Container
-    <Box flexDirection="column" alignItems="center" width="100%" height="100%" paddingTop={7}>
-      <Tabs showIndex={false} onChange={(tab) => setActiveTab(tab as Tab)}>
+    <Box flexDirection="column" alignItems="center" width="100%" height="100%" paddingTop={4}>
+      <Tabs
+        showIndex={false}
+        onChange={(tab) => {
+          setMessages([]); // Clear messages
+          setActiveTab(tab as Tab);
+        }}
+      >
         <Tab name="port">Port</Tab>
         <Tab name="market">Goods Market</Tab>
         <Tab name="shipyard">Shipyard</Tab>
@@ -75,6 +109,44 @@ export function GameScreen() {
             <Header day={context.day.toString()} currentPort={context.currentPort} balance={context.balance} />
             <Box width="100%" flexDirection="column">
               <BigText text="Market" font="tiny" />
+              {/* Prices */}
+              <Table
+                data={ports.map((port) => ({
+                  name: port,
+                  ...goods.reduce(
+                    (acc, good) => ({
+                      ...acc,
+                      [good]: `$${context.prices[port][good]}`,
+                    }),
+                    {},
+                  ),
+                }))}
+                headings={{ name: "" }}
+                // @ts-expect-error Table can't detect the goods
+                columns={[{ key: "name", align: "left" }, ...goods.map((good) => ({ key: good, align: "center" }))]}
+              />
+              {/* Hold */}
+              <Box marginY={1} gap={3}>
+                <Box flexDirection="column">
+                  <Text>Your hold:</Text>
+                  <Box flexDirection="column" borderStyle="round" paddingRight={3} paddingLeft={1}>
+                    {[...context.ship.hold.entries()].map(([good, quantity]) => (
+                      <Box key={good}>
+                        <Text>{good.padEnd(10, " ")}</Text>
+                        <Text>{quantity}</Text>
+                      </Box>
+                    ))}
+                  </Box>
+                </Box>
+                <Box flexDirection="column" alignItems="center">
+                  <Text>Available storage:</Text>
+                  <Text>
+                    {getAvailableStorage(context.ship)} / <Text>{context.ship.capacity}</Text>
+                  </Text>
+                </Box>
+              </Box>
+
+              <MarketForm />
             </Box>
           </Box>
         ) : activeTab === "shipyard" ? (
@@ -89,7 +161,9 @@ export function GameScreen() {
 
       {/* Display Messages */}
       <Box flexDirection="column" gap={1} marginTop={1}>
-        {message ? <Text>{message}</Text> : null}
+        {messages.map((message) => (
+          <Text key={message.id}>{message.text}</Text>
+        ))}
       </Box>
     </Box>
   );
@@ -119,6 +193,69 @@ function Header({ day, currentPort, balance }: { day: string; currentPort: Port;
           ${balance}
         </Text>
       </Text>
+    </Box>
+  );
+}
+
+function MarketForm() {
+  const context = GameContext.useSelector((snapshot) => snapshot.context);
+  const actor = GameContext.useActorRef();
+  const [action, setAction] = useState<"buy" | "sell" | undefined>();
+  const [good, setGood] = useState<Good | undefined>();
+  const [quantity, setQuantity] = useState(0);
+
+  return !action ? (
+    <Box flexDirection="column">
+      <Text>What would you like to do?</Text>
+      <Select
+        items={[
+          { value: "buy", label: "Buy" },
+          { value: "sell", label: "Sell" },
+        ]}
+        onSelect={({ value }) => setAction(value as "buy" | "sell")}
+      />
+    </Box>
+  ) : !good ? (
+    <Box flexDirection="column">
+      <Text>Which good do you wish to {action}?</Text>
+      <Select
+        items={goods.map((good) => ({ value: good, label: good }))}
+        onSelect={({ value }) => setGood(value as Good)}
+      />
+    </Box>
+  ) : (
+    <Box flexDirection="column">
+      {action === "buy" ? (
+        <Box marginBottom={1}>
+          <Text inverse>
+            You can afford {Math.floor(context.balance / context.prices[context.currentPort][good])} tons of{" "}
+            {good.toLowerCase()}.
+          </Text>
+        </Box>
+      ) : null}
+      <Box gap={1}>
+        <Text>
+          How much {good.toLowerCase()} would you like to {action}?
+        </Text>
+        <TextInput
+          placeholder="0"
+          onChange={(amount) => (isNaN(+amount) ? setQuantity(0) : setQuantity(+amount))}
+          onSubmit={() => {
+            if (!!action && !!good && !isNaN(quantity) && quantity > 0) {
+              if (action === "buy") {
+                actor.send({ type: "BUY_GOOD", good, quantity });
+              } else if (action === "sell") {
+                actor.send({ type: "SELL_GOOD", good, quantity });
+              }
+            }
+
+            // Reset form
+            setAction(undefined);
+            setGood(undefined);
+            setQuantity(0);
+          }}
+        />
+      </Box>
     </Box>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
-import { assign, emit, setup } from "xstate";
+import { assign, emit, not, setup } from "xstate";
+import { type Guard } from "xstate/guards";
 
 // ===== Constants =====
 export const goods = ["Wheat", "Tea", "Spices", "Opium", "Clay"] as const;
@@ -10,10 +11,18 @@ export const distanceMatrix: Record<Port, Record<Port, number>> = {
   Singapore: { "Hong Kong": 1460, Shanghai: 2279, Nagasaki: 2561, Singapore: 0, Manila: 1308 },
   Manila: { "Hong Kong": 706, Shanghai: 1307, Nagasaki: 1668, Singapore: 1308, Manila: 0 },
 } as const;
+const goodsInfo: { name: Good; basePrice: number; volatility: number }[] = [
+  { name: "Wheat", basePrice: 20, volatility: 0.1 },
+  { name: "Tea", basePrice: 50, volatility: 0.15 },
+  { name: "Spices", basePrice: 80, volatility: 0.2 },
+  { name: "Opium", basePrice: 150, volatility: 0.25 },
+  { name: "Clay", basePrice: 30, volatility: 0.05 },
+] as const;
 
 // ===== Types =====
-type Good = (typeof goods)[number];
+export type Good = (typeof goods)[number];
 export type Port = (typeof ports)[number];
+type Trend = "increasing" | "decreasing" | "stable";
 type Context = {
   currentPort: Port;
   day: number;
@@ -21,7 +30,11 @@ type Context = {
   ship: {
     health: number;
     speed: number;
+    capacity: number;
+    hold: Map<Good, number>;
   };
+  prices: Record<Port, Record<Good, number>>;
+  trends: Record<Port, Record<Good, Trend>>;
 };
 
 // ===== Utility Methods =====
@@ -29,23 +42,82 @@ export const calculateTravelTime = (from: Port, to: Port, shipSpeed: number) => 
   const distance = distanceMatrix[from][to];
   return Math.ceil(distance / shipSpeed);
 };
+const generateTrends = () =>
+  ports.reduce(
+    (accPorts, port) => ({
+      ...accPorts,
+      [port]: goods.reduce(
+        (accGoods, good) => ({
+          ...accGoods,
+          [good]: ["increasing", "decreasing", "stable"][Math.floor(Math.random() / 0.4)],
+        }),
+        {},
+      ),
+    }),
+    {} as Record<Port, Record<Good, Trend>>,
+  );
+const generatePrices = (trends: Record<Port, Record<Good, Trend>>) =>
+  ports.reduce(
+    (accPorts, port) => ({
+      ...accPorts,
+      [port]: goodsInfo.reduce((accGoods, good) => {
+        const trend = trends[port][good.name];
+        const trendFactor = trend === "increasing" ? 1.1 : trend === "decreasing" ? 0.9 : 1;
+        const randomFactor = 1 + (Math.random() - 0.5) * 2 * good.volatility; // Opium: [0.75, 1.25)
+        return { ...accGoods, [good.name]: Math.round(good.basePrice * trendFactor * randomFactor) };
+      }, {}),
+    }),
+    {} as Record<Port, Record<Good, number>>,
+  );
+const calculatePrice = ({
+  prices,
+  currentPort,
+  good,
+  quantity,
+}: {
+  prices: Record<Port, Record<Good, number>>;
+  currentPort: Port;
+  good: Good;
+  quantity: number;
+}) => prices[currentPort][good] * quantity;
+export const getAvailableStorage = (ship: Context["ship"]) =>
+  ship.capacity - [...ship.hold.values()].reduce((sum, tons) => sum + tons);
 
-const initialContext = () =>
-  ({
+const initialContext = () => {
+  const trends = generateTrends();
+  return {
     currentPort: "Hong Kong",
     day: 1,
     balance: 1000,
     ship: {
       health: 100,
       speed: 500,
+      capacity: 50,
+      hold: goods.reduce((map, good) => map.set(good, 0), new Map()),
     },
-  }) satisfies Context;
+    trends,
+    prices: generatePrices(trends),
+  } satisfies Context;
+};
 
+// See bug: https://github.com/statelyai/xstate/pull/4516
+// See workaround: https://github.com/statelyai/xstate/issues/5090#issuecomment-2388661190
 export const gameMachine = setup({
   types: {
     context: {} as Context,
-    events: {} as { type: "START_GAME" } | { type: "TRAVEL_TO"; destination: Port },
-    emitted: {} as { type: "message"; message: string },
+    events: {} as
+      | { type: "START_GAME" }
+      | { type: "TRAVEL_TO"; destination: Port }
+      | { type: "BUY_GOOD"; good: Good; quantity: number }
+      | { type: "SELL_GOOD"; good: Good; quantity: number },
+    emitted: {} as { type: "message"; message: string; timeout?: number | "auto" } | { type: "clearMessages" },
+  },
+  guards: {
+    canAfford: ({ context }, { good, quantity }: { good: Good; quantity: number }) =>
+      context.balance >= calculatePrice({ prices: context.prices, currentPort: context.currentPort, good, quantity }),
+    canStore: ({ context }, { quantity }: { quantity: number }) => quantity <= getAvailableStorage(context.ship),
+    enoughInHold: ({ context }, params: { good: Good; quantity: number }) =>
+      params.quantity > 0 && (context.ship.hold.get(params.good) || 0) >= params.quantity,
   },
 }).createMachine({
   initial: "introScreen",
@@ -70,9 +142,78 @@ export const gameMachine = setup({
                     context.day + calculateTravelTime(context.currentPort, event.destination, context.ship.speed),
                   ),
                 })),
-                emit(({ context }) => ({ type: "message", message: `Arrived to ${context.currentPort}` })),
+                emit({ type: "clearMessages" }),
+                emit(({ context }) => ({
+                  type: "message",
+                  message: `Arrived to ${context.currentPort}`,
+                  timeout: "auto",
+                })),
               ],
             },
+            BUY_GOOD: [
+              {
+                guard: not({
+                  // @ts-expect-error - xstate
+                  type: "canAfford",
+                  // @ts-expect-error - xstate
+                  params: ({ event }) => ({ good: event.good, quantity: event.quantity }),
+                }),
+                actions: [emit({ type: "message", message: "You don't have enough cash", timeout: "auto" })],
+              },
+              {
+                guard: not({
+                  // @ts-expect-error - xstate
+                  type: "canStore",
+                  // @ts-expect-error - xstate
+                  params: ({ event }) => ({ quantity: event.quantity }),
+                }),
+                actions: [emit({ type: "message", message: "You don't have enough storage room", timeout: "auto" })],
+              },
+              {
+                actions: [
+                  assign(({ context, event }) => ({
+                    balance:
+                      context.balance -
+                      calculatePrice({
+                        prices: context.prices,
+                        currentPort: context.currentPort,
+                        good: event.good,
+                        quantity: event.quantity,
+                      }),
+                    hold: context.ship.hold.set(event.good, (context.ship.hold.get(event.good) ?? 0) + event.quantity),
+                  })),
+                ],
+              },
+            ],
+            SELL_GOOD: [
+              {
+                guard: {
+                  type: "enoughInHold",
+                  params: ({ event }) => ({ good: event.good, quantity: event.quantity }),
+                },
+                actions: [
+                  assign(({ context, event }) => ({
+                    balance:
+                      context.balance +
+                      calculatePrice({
+                        prices: context.prices,
+                        currentPort: context.currentPort,
+                        good: event.good,
+                        quantity: event.quantity,
+                      }),
+                    hold: context.ship.hold.set(event.good, (context.ship.hold.get(event.good) ?? 0) - event.quantity),
+                  })),
+                ],
+              },
+              {
+                actions: [
+                  emit(({ event }) => ({
+                    type: "message",
+                    message: `You don't have enough ${event.good.toLowerCase()} to sell.`,
+                  })),
+                ],
+              },
+            ],
           },
         },
       },
